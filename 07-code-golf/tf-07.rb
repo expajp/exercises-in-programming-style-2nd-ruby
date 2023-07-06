8d823ebecb75d4ed20d38f81a00b9de96daf7d43
Shu Oogawara <expajp@gmail.com>
2023-07-07 00:54:49 +0900

    07 use numbered parameter

--- 07-code-golf/tf-07.rb
+++ 07-code-golf/tf-07.rb
@@ -6,7 +6,7 @@ open(ARGV[0])
     .read
     .downcase
     .scan(/[a-z]{2,}/)
-    .each_with_object(Hash.new 0) { |w, h| h[w] += 1 if !stops.include? w }
+    .each_with_object(Hash.new 0) { _2[_1] += 1 if !stops.include? _1 }
     .to_a
-    .sort_by.with_index { |a, i| [-a[1], i] }[0..24]
-    .each { |k, v| print "#{k} - #{v}\n" }
+    .sort_by.with_index { [-_1[1], _2] }[0..24]
+    .each { print "#{_1} - #{_2}\n" }
