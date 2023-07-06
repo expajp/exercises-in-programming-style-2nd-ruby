c42a643e28f2513ba16251f9b6a8706c3fe89cc1
Shu Oogawara <expajp@gmail.com>
2023-07-07 00:56:46 +0900

    07 remove spaces

--- 07-code-golf/tf-07.rb
+++ 07-code-golf/tf-07.rb
@@ -6,7 +6,7 @@ open(ARGV[0])
     .read
     .downcase
     .scan(/[a-z]{2,}/)
-    .each_with_object(Hash.new 0) { _2[_1] += 1 if !stops.include? _1 }
+    .each_with_object(Hash.new 0){_2[_1] += 1 if !stops.include? _1}
     .to_a
-    .sort_by.with_index { [-_1[1], _2] }[0..24]
-    .each { print "#{_1} - #{_2}\n" }
+    .sort_by.with_index{[-_1[1], _2]}[0..24]
+    .each{print "#{_1} - #{_2}\n"}
