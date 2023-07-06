03842026421db53e08e24ea1a28aeda97db33c16
Shu Oogawara <expajp@gmail.com>
2023-07-07 00:58:13 +0900

    07 change name of variable

--- 07-code-golf/tf-07.rb
+++ 07-code-golf/tf-07.rb
@@ -1,12 +1,12 @@
 raise 'Please assign an input file as ARGV[0]' if ARGV[0].nil?
 raise 'Please assign an stop_words file as ARGV[1]' if ARGV[1].nil?
 
-stops = open(ARGV[1]).read.split(',')
+_ = open(ARGV[1]).read.split(',')
 open(ARGV[0])
     .read
     .downcase
     .scan(/[a-z]{2,}/)
-    .each_with_object(Hash.new 0){_2[_1] += 1 if !stops.include? _1}
+    .each_with_object(Hash.new 0){_2[_1] += 1 if !_.include? _1}
     .to_a
     .sort_by.with_index{[-_1[1], _2]}[0..24]
     .each{print "#{_1} - #{_2}\n"}
