9a5d1e606b1957280b69ee736aa28f235c3e1e27
Shu Oogawara <expajp@gmail.com>
2024-03-23 23:11:33 +0900

    08 add comments

--- 08-infinite-mirror/tf-08.rb
+++ 08-infinite-mirror/tf-08.rb
@@ -5,6 +5,8 @@ def count(word_list, stop_words, word_freqs)
     # 空のリストであった場合
     return if word_list.empty?
 
+    # 再帰的な場合、つまりリストに単語が入っている場合
+    # リスト先頭の単語を処理する
     word = word_list[0]
     if !stop_words.include?(word)
         if word_freqs.include?(word)
@@ -13,6 +15,8 @@ def count(word_list, stop_words, word_freqs)
             word_freqs[word] = 1
         end
     end
+
+    # 残りを処理する
     count(word_list[1..], stop_words, word_freqs)
 end
 
@@ -31,7 +35,8 @@ stop_words = File.open(ARGV[1]).read.split(',')
 word_list = File.open(ARGV[0]).read.gsub(/[\W_]+/, ' ').downcase.split(' ').select{ _1.match?(/[a-z]{2,}/) }
 word_freqs = {}
 
-0.step(word_list.count, recursion_limit) do |i|
-    count(word_list[i..i+recursion_limit-1], stop_words, word_freqs)
-end
+# スタックを使い果たすのを防ぐためrecusrsion_limit ごとに処理
+# 本来はcountを呼び出すだけで良い
+0.step(word_list.count, recursion_limit) { |i| count(word_list[i..i+recursion_limit-1], stop_words, word_freqs) }
+
 wf_print(word_freqs.to_a.sort_by.with_index { |v, i| [-v[1], i += 1] }[0..24])
