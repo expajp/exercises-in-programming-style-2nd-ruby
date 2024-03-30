019561f3a6739d3e0bf1d9545d13a4e930b696b5
Shu Oogawara <expajp@gmail.com>
2024-03-30 11:28:12 +0900

    11 change class variables to private attr_accessors

--- 11-things/tf-11.rb
+++ 11-things/tf-11.rb
@@ -9,73 +9,89 @@ class TFExercise
 end
 
 class DataStorageManager < TFExercise
-
+    
     def initialize(path_to_file)
-        @data = File.open(path_to_file).read.gsub(/[\W_]+/, ' ').downcase
+        self.data = File.open(path_to_file).read.gsub(/[\W_]+/, ' ').downcase
     end
     
     def words
-        @data.split(' ')
+        self.data.split(' ')
     end
     
     def info
-        "#{super.info}: My major data structure is a #{@data.class.name}"
+        "#{super.info}: My major data structure is a #{self.data.class.name}"
     end
+
+    private
+
+    attr_accessor :data
 end
 
 class StopWordManager < TFExercise
     
     def initialize(path_to_stop_words_file)
-        @stop_words = File.open(path_to_stop_words_file).read.split(',')
-        @stop_words += ('a'..'z').to_a
+        self.stop_words = File.open(path_to_stop_words_file).read.split(',')
+        self.stop_words += ('a'..'z').to_a
     end
     
     def stop_word?(word)
-        @stop_words.include?(word)
+        self.stop_words.include?(word)
     end
     
     def info
-        "#{super.info}: My major data structure is a #{@stop_words.class.name}"
+        "#{super.info}: My major data structure is a #{self.stop_words.class.name}"
     end
+
+    private
+
+    attr_accessor :stop_words
 end
 
 class WordFrequencyManager < TFExercise
     
     def initialize
-        @word_freqs = Hash.new(0)
+        self.word_freqs = Hash.new(0)
     end
     
     def increment_count(word)
-        @word_freqs[word] += 1
+        self.word_freqs[word] += 1
     end
     
     def sorted
-        @word_freqs.to_a.sort_by.with_index { |v, i| [-v[1], i += 1] }
+        self.word_freqs.to_a.sort_by.with_index { |v, i| [-v[1], i += 1] }
     end
 
     def info
-        "#{super.info}: My major data structure is a #{@word_freqs.class.name}"
+        "#{super.info}: My major data structure is a #{self.word_freqs.class.name}"
     end
+
+    private
+
+    attr_accessor :word_freqs
 end
 
 class WordFrequencyController < TFExercise
 
     def initialize(path_to_file, path_to_stop_words_file)
-        @storage_manager = DataStorageManager.new(path_to_file)
-        @stop_word_manager = StopWordManager.new(path_to_stop_words_file)
-        @word_freq_manager = WordFrequencyManager.new
+        self.storage_manager = DataStorageManager.new(path_to_file)
+        self.stop_word_manager = StopWordManager.new(path_to_stop_words_file)
+        self.word_freq_manager = WordFrequencyManager.new
     end
 
     def run
-        @storage_manager.words.each do |word|
-            if !@stop_word_manager.stop_word?(word)
-                @word_freq_manager.increment_count(word)
+        self.storage_manager.words.each do |word|
+            if !self.stop_word_manager.stop_word?(word)
+                self.word_freq_manager.increment_count(word)
             end
         end
 
-        word_freqs = @word_freq_manager.sorted
+        word_freqs = self.word_freq_manager.sorted
         word_freqs[0..24].each { |k, v| print("#{k} - #{v}\n") }
     end
+
+    private
+
+    attr_accessor :storage_manager, :stop_word_manager, :word_freq_manager
 end
 
 WordFrequencyController.new(ARGV[0], ARGV[1]).run
