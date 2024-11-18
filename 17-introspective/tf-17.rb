19b19fa938a25ae224b00e03b28beb2e70a1edb2
Shu Oogawara <expajp@gmail.com>
2024-11-18 10:24:15 +0900

    17 change local variables usage to Binding\#local_variable_get instead of locals() in python

--- 17-introspective/tf-17.rb
+++ 17-introspective/tf-17.rb
@@ -8,11 +8,11 @@ end
 def read_stop_words(path_to_stop_words_file)
     return if caller_method != 'extract_words'
 
-    File.open(path_to_stop_words_file).read.split(',') + ('a'..'z').to_a
+    File.open(binding.local_variable_get('path_to_stop_words_file')).read.split(',') + ('a'..'z').to_a
 end
 
 def extract_words(path_to_file, path_to_stop_words_file)
-    word_list = File.open(path_to_file).read.gsub(/[\W_]+/, ' ').downcase.split(' ')
+    word_list = File.open(binding.local_variable_get('path_to_file')).read.gsub(/[\W_]+/, ' ').downcase.split(' ')
     stop_words = read_stop_words(path_to_stop_words_file)
 
     word_list - stop_words
@@ -20,7 +20,7 @@ end
 
 def frequencies(word_list)
     word_freqs = Hash.new(0)
-    word_list.each do |w|
+    binding.local_variable_get('word_list').each do |w|
         word_freqs[w] += 1
     end
 
@@ -34,7 +34,7 @@ end
 def main
     word_freqs = sort(frequencies(extract_words(ARGV[0], ARGV[1])))
 
-    word_freqs[0..24].each do |w, c|
+    binding.local_variable_get('word_freqs')[0..24].each do |w, c|
         print("#{w} - #{c}\n")
     end
 end
